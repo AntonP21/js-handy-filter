760ad9d927a051dc31459353aa43e060cdfc8bb7
Anton <polovikhin@fas.gov.ru>
2021-03-14 00:20:09 +0300

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      conditions: path.resolve(__dirname, 'src/conditions/'),
+    },
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+};
