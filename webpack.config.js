7c540712129fd24cc0c6ca3b7f60e01aa5e092a1
Anton <polovikhin@fas.gov.ru>
2021-04-04 03:10:05 +0300

    Release 1.0.4 Settings for npm;

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const tsTransformPaths = require('@zerollup/ts-transform-paths');
+
+module.exports = {
+  mode: 'production',
+  entry: './index.ts',
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          getCustomTransformers: (program) => {
+            const transformer = tsTransformPaths(program);
+            return { before: [transformer.before], afterDeclarations: [transformer.afterDeclarations] };
+          },
+        },
+      },
+    ],
+  },
+
+  resolve: {
+    alias: {
+      'condition-parser': path.resolve(__dirname, 'src/condition-parser/'),
+      conditions: path.resolve(__dirname, 'src/conditions/'),
+      filter: path.resolve(__dirname, 'src/filter/'),
+      lib: path.resolve(__dirname, 'src/lib/'),
+    },
+    extensions: ['.ts', '.js'],
+  },
+
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+
+  watchOptions: {
+    ignored: '/node_modules/',
+  },
+};
